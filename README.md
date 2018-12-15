2ef52d18b053542c14dad24ee344ff5de24b30d7
Luiz Augusto Vilela Marques <luizvilelamarques@gmail.com>
2018-12-15 14:02:06 -0200

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+#instalar as dependencias
+ npm install
+
+#rodar o serverless off line
+ sls offline start
+
+fazer deploy do serverless
+ sls deploy
+ sls info
+ 
---
+++ handler.js
@@ -0,0 +1,43 @@
+'use strict';
+const AssistantV1 = require('watson-developer-cloud/assistant/v1');
+
+module.exports.conversation = (event, context, callback) => {
+
+  try {	
+    const data = JSON.parse(event.body);	
+    const assistant = new AssistantV1({
+      username: process.env.ASSISTANT_USERNAME,
+      password: process.env.ASSISTANT_PASSWORD,
+      url: 'https://gateway.watsonplatform.net/assistant/api/',
+      version: '2018-09-19',
+    });
+
+    const params = {
+      input: { 'text': data.text},
+      workspace_id: process.env.WORKSPACE_ID,
+      context: data.context,
+    };
+  
+    assistant.message(params, (err, res) => {
+      if (err) {
+	    callback(null, {
+           statusCode: 501,
+           headers: { 'Content-Type': 'text/plain' },
+           body: JSON.stringify(err),
+        });
+      } else {
+        const response = {
+	      statusCode: 200,
+	      body: JSON.stringify(res)
+	    };
+	    callback(null, response);
+	  }
+    });
+  } catch (e) {
+    callback(null, {
+      statusCode: 400,
+      headers: { 'Content-Type': 'text/plain' },
+      body: 'Couldn\'t parse json body',
+    });
+  }	
+};
